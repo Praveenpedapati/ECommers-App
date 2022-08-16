68130f7735e8311bdfc598db5bd6ea7d1b338e02
John Doe <soumya.antipeta@rampgroup.com>
2022-08-16 16:28:33 +0530

    First commit

--- app/src/main/java/com/example/ecommerceapp/RegistrationActivity.java
+++ app/src/main/java/com/example/ecommerceapp/RegistrationActivity.java
@@ -29,8 +29,12 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         getSupportActionBar().hide();
-
         auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null){
+            startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+            finish();
+        }
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
